90d7037ef9d3fa03d479ffe8047d0cb5836dbf0b
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2023-09-13 18:12:35 -0400

    atividade 01

--- aula31082023/Atividade/GuicheVoo/GuicheVoo/Program.cs
+++ aula31082023/Atividade/GuicheVoo/GuicheVoo/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Voo passagem;
+int quantAcentos = 0;
+bool cond = false;
+Console.Write("===================================\n" +
+    "Digite a quantidade de Acentos que possui o Aviao: ");
+string acentos = Console.ReadLine();
+if (!int.TryParse(acentos, out quantAcentos))
+{
+    cond = true;
+}
+if (quantAcentos > 99 || quantAcentos < 10)
+{
+    cond = true;
+}
+while (cond)
+{
+    Console.Clear();
+    cond = false;
+    Console.WriteLine("===Digite um Numero válido====");
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("!! Não pode ser menor que 10 acentos e o limite é de 99 acentos !!");
+    Console.ResetColor();
+    Console.WriteLine("Digite a quantidade de Acentos que possui o Aviao: ");
+    acentos = Console.ReadLine();
+    if (!int.TryParse(acentos, out quantAcentos))
+    {
+        cond = true;
+    }
+    if (quantAcentos > 99 || quantAcentos < 10)
+    {
+        cond = true;
+    }
+}
+passagem = new Voo(2,09,2023,15,quantAcentos);
+Console.Write("#Ocupar Acento: ");
+int acento = 0;
+try
+{
+    acento = Convert.ToInt32(Console.ReadLine());
+}
+catch (Exception)
+{
+    Console.WriteLine("Apenas Numeros...");
+    throw;
+}
+if (acento < 1 || acento > quantAcentos)
+{
+    Console.WriteLine("Acento Inválido");
+}
+else if (passagem.Verifica(acento - 1))
+{
+    Console.WriteLine("Acento Ocupado");
+    Console.WriteLine("Acentos Livre:\n"+passagem.AcentosVoo());
+}
+else
+{
+    passagem.Ocupa(acento - 1);
+    Console.WriteLine($"\n====== COMPROVANTE ======\n" +
+                     $"        Acento {acento}         \n" +
+                     $"        Voo {passagem.getNumeroVoo()}      \n" +
+                     $"    {passagem.getDataVoo()} - {passagem.getHoraVoo()}\n" +
+                     $"==========================");
+}
+
+
+Console.WriteLine("\nAcentos Aviao:\n" + passagem.AcentosVoo() +"\n" +
+    $"Quantidade de Acentos Livres: {passagem.Vagas()}");
---
+++ aula31082023/Atividade/GuicheVoo/GuicheVoo/Voo.cs
@@ -0,0 +1,182 @@
+﻿public class Voo
+{
+    private int numPassagens = 0, numeroVoo = 0;
+    private DateTime dataVoo = new DateTime();
+    private bool[] acentos = {  };
+    public Voo(int diaDoVoo, int mesDoVoo, int anoDoVoo, int horaDoVoo, int quantPassagensVoo)
+    {
+        setNumeroVoo();
+        setDataVoo(diaDoVoo,mesDoVoo, anoDoVoo, horaDoVoo);
+        setNumPassagens(quantPassagensVoo);
+    }
+    public void setNumeroVoo()
+    {
+        Random random = new Random();
+        string numero = "";
+        for (int i = 0; i < 4; i++)
+        {
+            numero += Convert.ToString(random.Next(1, 9));
+        }
+        this.numeroVoo = Convert.ToInt32(numero);
+    }
+    public int getNumeroVoo()
+    {
+        return this.numeroVoo;
+    }
+    public void setDataVoo(int dia, int mes, int ano, int hora)
+    {
+        this.dataVoo = new DateTime(ano, mes, dia, hora, DateTime.Now.Minute, DateTime.Now.Second);
+    }
+    public string getDataVoo()
+    {
+        string dataDoVoo = "";
+        dataDoVoo = $"{this.dataVoo.Day}/{this.dataVoo.Month}/{this.dataVoo.Year}";
+        return dataDoVoo;
+    }
+    public string getHoraVoo()
+    {
+        string horaDoVoo = "";
+        horaDoVoo = $"{this.dataVoo.Hour}:{this.dataVoo.Minute}:{this.dataVoo.Second}";
+        return horaDoVoo;
+    }
+    public void setNumPassagens(int quantidade)
+    {
+        this.numPassagens = quantidade;
+        this.acentos = new bool[numPassagens];
+        for (int i = 0; i < acentos.Length; i++)
+        {
+            acentos[i] = false;
+        }
+    }
+    public int ProximoLivre(int acentoAtual)
+    {
+        for (int i = acentoAtual; i < acentos.Length; i++)
+        {
+            if (acentos[i] == false)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    public bool Verifica(int numCadeira)
+    {
+        if (acentos[numCadeira] == true)
+        {
+            return true;
+        }
+        return false;
+    }
+    public void Ocupa(int numCadeira)
+    {
+        acentos[numCadeira] = true;
+    }
+    public int Vagas()
+    {
+        int contVagas = 0;
+        for (int i = 0; i < acentos.Length; i++)
+        {
+            if (acentos[i] == false)
+            {
+                contVagas++;
+            }
+        }
+
+        return contVagas;
+    }
+    public string AcentosVoo()
+    {
+        string aviao = $"+========================+\n| ";
+        int contColu = 0;
+        for (int i = 0; i < acentos.Length; i++)
+        {
+            if (i < 9)
+            {
+                if (contColu < 4)
+                {
+                    if (contColu != 2)
+                    {
+                        if (acentos[i] == false)
+                        {
+                            aviao += $"[\u001b[32m0{i+1}\u001b[0m]";
+                        }
+                        else
+                        {
+                            aviao += $"[\u001b[31m0{i + 1}\u001b[0m]";
+                        }   
+                    }
+                    else
+                    {
+                        if (acentos[i] == false)
+                        {
+                            aviao += $" [\u001b[32m0{i + 1}\u001b[0m] ";
+                        }
+                        else
+                        {
+                            aviao += $" [\u001b[31m0{i + 1}\u001b[0m] ";
+                        }
+                    }
+                    contColu++;
+                }
+                else
+                {
+                    contColu = 0;
+                    if (acentos[i] == false)
+                    {
+                        aviao += $"[\u001b[32m0{i + 1}\u001b[0m]";
+                    }
+                    else
+                    {
+                        aviao += $"[\u001b[31m0{i + 1}\u001b[0m]";
+                    }
+                    aviao += $" |\n| ";
+                }
+            }
+            else
+            {
+                if (contColu < 4)
+                {
+                    if (contColu != 2)
+                    {
+                        if (acentos[i] == false)
+                        {
+                            aviao += $"[\u001b[32m{i + 1}\u001b[0m]";
+                        }
+                        else
+                        {
+                            aviao += $"[\u001b[31m{i + 1}\u001b[0m]";
+                        }
+                    }
+                    else
+                    {
+                        if (acentos[i] == false)
+                        {
+                            aviao += $" [\u001b[32m{i + 1}\u001b[0m] ";
+                        }
+                        else
+                        {
+                            aviao += $" [\u001b[31m{i + 1}\u001b[0m] ";
+                        }
+                    }
+                    contColu++;
+                }
+                else
+                {
+                    contColu = 0;
+                    if (acentos[i] == false)
+                    {
+                        aviao += $"[\u001b[32m{i + 1}\u001b[0m]";
+                    }
+                    else
+                    {
+                        aviao += $"[\u001b[31m{i + 1}\u001b[0m]";
+                    }
+                    aviao += $" |\n| ";
+                }
+            }
+        }
+
+        return aviao;
+    }
+}
